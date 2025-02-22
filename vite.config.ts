f36f53322aa2647ab768ba2ac61572b29329119b
vin <ldvinh596@gmail.com>
2025-02-22 15:33:36 +0700

    feature: add alias import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
+
+import { fileURLToPath } from 'url'
+
+// Convert import.meta.url to __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +17,10 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
