022d4079e0b537f4e9706f519c25086c90c1d78a
vin <ldvinh596@gmail.com>
2025-02-22 15:24:31 +0700

    feature: add tailwind css and editor config

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+export default {
+  darkMode: ['class'],
+  content: ['./pages/**/*.{ts,vue}', './components/**/*.{ts,vue}', './src/**/*.{ts,vue}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      colors: {},
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
+  },
+}
